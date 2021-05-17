f1b4e4a337d050b67612abe9d80e8e1084698f91
rlatnqls11 <subin0940@gmail.com>
2021-05-17 20:42:09 +0900

    오목 색상

---
+++ omok_colors.py
@@ -0,0 +1,10 @@
+"""
+colors module. each color is represented as a triplet of int (0-255)
+"""
+BLUE = (0, 0, 255)
+NAVY = (0, 128, 128)
+RED = (200, 0, 0)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+PURPLE = (0, 77, 64)
