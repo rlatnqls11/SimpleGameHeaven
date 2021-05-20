eecf7a47714d07185c27f6819c28f190dc5378a7
KDH5602 <kdh5602@gmail.com>
2021-05-20 22:01:10 +0900

    노래 on/off/ 버튼 구현

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ bglabel.pack()
 def windowquit():
     mySound.stop()
     window.destroy()
+
+def soundstop():
+    mySound.stop()
+    Sob['text'] = "노래 켜기"
+    Sob['command'] = soundstart
+
+def soundstart():
+    mySound.play(-1)
+    Sob['text'] = "노래 끄기"
+    Sob['command'] = soundstop
     
 Sb = Button(window, text = "게임 시작", fg = "white", bg = "black", width =10, height=2)
 Sb.pack()
@@ -32,6 +42,10 @@ Hb.place(x=340,y=210)
 Eb = Button(window, text = "게임 나가기" ,fg = "white", bg = "red", command = windowquit, width =10, height=2)
 Eb.pack()
 Eb.place(x=340,y=270)
+
+Sob = Button(window, text = "노래 켜기", command = soundstop ,fg = "white", bg = "pink", width =8, height=1)
+Sob.pack()
+Sob.place(x=0,y=0)
 
 window.protocol('WM_DELETE_WINDOW', window)
 
